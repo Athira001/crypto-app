4741ae741e2955a44ffb2a0c2b1c55f79b594be8
Tushar Agarwal <niftytushar@gmail.com>
2018-03-31 12:30:14 +0530

    Setup eslint airbnb

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,3 +1,4 @@
+/* eslint no-use-before-define: 0 */  // --> OFF
 // In production, we register a service worker to serve assets from local cache.
 
 // This lets the app load faster on subsequent visits in production, and gives
